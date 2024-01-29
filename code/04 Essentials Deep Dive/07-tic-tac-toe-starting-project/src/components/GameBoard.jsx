1eafa100c8195897e5fbfb870c5fbadde1984b68
ted zhang <ted.zhang666@gmail.com>
2024-01-28 23:36:43 -0500

    tic-tac-toe game: adding gameboard

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,5 @@
+export default function GameBoard() {
+    return <ol id="game-board">
+        <li></li>
+    </ol>
+}
--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,9 +1,33 @@
-export default function Player({name, symbol}) {
-    return <li>
-    <span>
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-    </span>
-    <button>Edit</button>
-  </li>
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((prevState) => !prevState);
+  };
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let btnCaption = 'Edit';
+
+  if (isEditing) {
+    btnCaption = 'Save';
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input type="text" required value={playerName} onChange={handleChange}></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
 }
